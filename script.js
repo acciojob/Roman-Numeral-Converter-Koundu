8c0084c3ee634c7f8310bf6ff31a40232fc1067a
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-25 15:00:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ function convertToRoman(num) {
     };
 
   //your code here
+	    var roman = '';
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    for (var i = 0; i < decimal.length; i++) {
+        while (decimal[i] <= num) {
+            roman += romanNum[i];
+            num -= decimal[i];
+        }
+    }
+    return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
